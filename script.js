e3f5482293cca2bb67361c74b24d42f8ecae9d0d
Ashfatul Islam <59108821+Ashfatul@users.noreply.github.com>
2023-05-12 17:35:35 +0600

    trusted client slider

---
+++ script.js
@@ -0,0 +1,41 @@
+$(".slider").slick({
+   dots: false,
+   infinite: true,
+   speed: 500,
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   arrows: true,
+   //    mobileFirst: true,
+   autoplay: true,
+   responsive: [
+      {
+         breakpoint: 1080,
+         settings: {
+            slidesToShow: 4,
+         },
+      },
+
+      {
+         breakpoint: 992,
+         settings: {
+            slidesToShow: 3,
+         },
+      },
+
+      {
+         breakpoint: 768,
+         settings: {
+            slidesToShow: 2,
+            arrows: true,
+         },
+      },
+
+      {
+         breakpoint: 520,
+         settings: {
+            slidesToShow: 1,
+            arrows: false,
+         },
+      },
+   ],
+});
